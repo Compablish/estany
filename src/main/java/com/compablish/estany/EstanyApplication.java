ca859b099a26efe89182ce7d98c58c22ebcc20ff
Jin Byeon <compablish@gmail.com>
2023-02-15 13:58:03 +0900

    first commit

---
+++ src/main/java/com/compablish/estany/EstanyApplication.java
@@ -0,0 +1,13 @@
+package com.compablish.estany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstanyApplication.class, args);
+    }
+
+}
